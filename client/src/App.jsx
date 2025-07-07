ff2deec9a0dd761b7e0597a1cb82b7865c9ceeb6
Avinash Gupta <avinashkumargupta306@gmail.com>
2025-07-07 20:20:08 +0530

    small changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,8 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/profile" element={<ProfessionalProfilePage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+
         </Routes>
       </Router>
     </AuthProvider>
